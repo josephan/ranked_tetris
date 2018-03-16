6d783234649d5c82c3a32fc95eea65588c9b94fa
Joseph An <joseph.an@thescore.com>
2018-03-16 19:02:59 -0400

    Add seeds for local testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-u = User.create(name: "Joseph", email: "joseph@thescore.com", elo: 1500)
-u2 = User.create(name: "Stevens", email: "ripstevens@thescore.com", elo: 3000)
+User.destroy_all
+Match.destroy_all
+
+u2 = User.create(name: "Stevens (RIP)", email: "stevens@thescore.com", elo: 10000)
+u = User.create(name: "Joseph", email: "joseph@thescore.com", elo: 9000)
+User.create(name: "McKay", email: "mckay@thescore.com", elo: 8000)
+User.create(name: "Thorney", email: "thorney@thescore.com", elo: 7000)
+User.create(name: "Gough", email: "gough@thescore.com", elo: 6000)
+User.create(name: "Sacha", email: "sacha@thescore.com", elo: 5000)
+User.create(name: "Kuba", email: "kuba@thescore.com", elo: 4000)
 
 Match.create(player_one: u, player_two: u2, winner: u2)
