46874ddd3ca76795ac61a41bc8a86343123ab015
Joseph An <joseph.an@thescore.com>
2018-03-16 18:38:17 -0400

    Add model empty model test cases

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe User, :type => :model do
+  describe "#won_matches" do
+    it "returns all matches that the user has won" do
+
+    end
+  end
+
+  describe "#matches" do
+    it "returns all matches that the user has played" do
+
+    end
+  end
+
+  describe "#wins" do
+    it "returns the total number of matches the user has won" do
+
+    end
+  end
+
+  describe "#losses" do
+    it "returns the total number of matches the player has lost" do
+
+    end
+  end
+end
