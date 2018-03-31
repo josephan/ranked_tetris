e4108adff63c50dad2131ac5d5863385dd956528
Joseph An <an.joseph891@gmail.com>
2018-03-31 02:43:06 -0400

    Add emoji in ranking table

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
 module PagesHelper
+  def medal(index, count)
+    case index
+    when 0
+      "🥇"
+    when 1
+      "🥈"
+    when 2
+      "🥉"
+    when count - 1
+      "💩"
+    else
+      nil
+    end
+  end
 end
