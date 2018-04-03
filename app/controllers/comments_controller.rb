01253ac726066bccc6233de0ca0e3afca2af46b2
Joseph An <joseph.an@thescore.com>
2018-04-03 02:53:19 -0400

    Add alerts for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,10 +5,17 @@ class CommentsController < ApplicationController
 
   def create
     @comment = Comment.new(comment_params.merge(user: current_user))
+    @match = Match.includes(:player_one, :player_two).find(comment_params[:match_id])
+    GhettoSlack.send_to_user(@match.player_one, message) if @match.player_one != current_user
+    GhettoSlack.send_to_user(@match.player_two, message) if @match.player_two != current_user
     @comment.save
   end
 
   private
+
+  def message
+    "#{current_user.first_and_last} just commented in your match! (<#{@match.url}|read comment>)"
+  end
 
   def comment_params
     params.require(:comment).permit(:body, :match_id)
