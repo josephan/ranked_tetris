d29c1d9be211a130bd07b2481f2e0463788be09a
Joseph An <joseph.an@thescore.com>
2018-04-03 20:08:33 -0400

    Add mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "ranked_tetris_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { :host => "www.ranked.fun" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.sendgrid.net',
